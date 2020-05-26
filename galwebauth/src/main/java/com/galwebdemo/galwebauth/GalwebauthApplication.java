7209eaabc053ba26d170a9e4dd69b6f274f7779e
mcygoogle <mcynever@google.com>
2020-05-26 17:43:45 -0400

    first commit

---
+++ galwebauth/src/main/java/com/galwebdemo/galwebauth/GalwebauthApplication.java
@@ -0,0 +1,13 @@
+package com.galwebdemo.galwebauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalwebauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalwebauthApplication.class, args);
+	}
+
+}
